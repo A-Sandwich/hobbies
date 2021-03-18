1ec9904dc4e2c902a5ea767ad1d1f26759bcf1af
Kyle <kbburkholder@gmail.com>
2021-03-17 22:27:01 -0400

    Started appppppppparooney

---
+++ hobbies/contentConsumed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ContentconsumedConfig(AppConfig):
+    name = 'contentConsumed'
--- hobbies/hobbies/settings.py
+++ hobbies/hobbies/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contentConsumed.apps.ContentconsumedConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'hobbies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hobbies',
+        'USER': 'django',
+        'PASSWORD': 'cheese19',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
