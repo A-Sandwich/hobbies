200fa718baf32d79892b291515f017fed2fa380f
A_Sandwich <kbburkholder@gmail.com>
2021-07-10 10:49:50 -0400

    Fixing debug level

--- hobbies/hobbies/settings.py
+++ hobbies/hobbies/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 if (os.environ.get('ENVIRONMENT') and os.environ.get('ENVIRONMENT') == 'DEVELOPMENT'):
     DEBUG = True
-DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://www.beepboop.info/', 'pure-reaches-67514.herokuapp.com']
 
