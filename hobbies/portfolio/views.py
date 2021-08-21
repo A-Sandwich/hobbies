cded41734dde29ff6d90bc63481372d8bb32398e
Kyle <kbburkholder@gmail.com>
2021-08-20 22:05:38 -0400

    Added miles to template

--- hobbies/portfolio/views.py
+++ hobbies/portfolio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from random import choice, seed
 from .helper import Helper
 from .popomodels import POPOImage
+from stats.models import ExerciseTotal
+from datetime import date
 
 def index(request):
     helper = Helper()
@@ -10,13 +12,18 @@ def index(request):
     food_image = helper.select_static_file('food')
     other_image = helper.select_static_file('other')
     color = helper.choose_css_color()
+    running_exercise = ExerciseTotal.objects.filter(year=str(date.today().year), exercise_type="running").first()
+    distance = 0
+    if running_exercise:
+        distance = running_exercise.get_miles()
 
     return render(request, 'index.html', {
         'backgroundcolor': color,
         'primary_image': primary_image,
         'programming_image': programming_image,
         'food_image': food_image,
-        'other_image': other_image
+        'other_image': other_image,
+        'distance': round(distance, 1)
     })
 
 
